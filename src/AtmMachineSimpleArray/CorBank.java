183ef686493431d855c4ef36b4900ae19b7d11a7
Aidre Cabrera <kontradikta1986@gmail.com>
2022-05-16 23:32:24 +0800

    Cor Bank ATM Machine

--- src/AtmMachineSimpleArray/CorBank.java
+++ src/AtmMachineSimpleArray/CorBank.java
@@ -9,7 +9,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -45,17 +47,25 @@ public class CorBank extends Application {
     private TextField thisInputNumber;
 
     @FXML
-    private TextField thisInputPin;
+    private PasswordField thisInputPin;
 
-    ArrayList<String> arrUsername = new ArrayList<>();
-    ArrayList<Integer> arrUserpin = new ArrayList<>();
-    ArrayList<Double> arrCheckingBalance = new ArrayList<>();
-    ArrayList<Double> arrSavingsBalance = new ArrayList<>();
+    @FXML
+    public Text thisCheckingBalance;
+
+    @FXML
+    public Text thisSavingsBalance;
+
+    public static ArrayList<String> arrUsername = new ArrayList<>();
+    public static ArrayList<Integer> arrUserpin = new ArrayList<>();
+    public static ArrayList<Double> arrCheckingBalance = new ArrayList<>();
+    public static ArrayList<Double> arrSavingsBalance = new ArrayList<>();
+    public static int indexReference = 0;
 
     @FXML
     void loginClick(ActionEvent actionEvent) {
         String inputUsername = thisInputNumber.getText();
         int inputUserpin = Integer.parseInt(thisInputPin.getText());
+        indexReference = arrUsername.indexOf(inputUsername);
         if (arrUsername.contains(inputUsername) && arrUserpin.contains(inputUserpin)) {
             JOptionPane.showMessageDialog(null, "Successfully Logged in. Welcome to Cor Bank!");
             try {
@@ -89,12 +99,6 @@ public class CorBank extends Application {
     }
 
     @FXML
-    private Label thisCheckingBalance;
-
-    @FXML
-    private Label thisSavingsBalance;
-
-    @FXML
     void registerClick(ActionEvent event) {
         String inputUsername = thisInputNumber.getText();
         int inputUserpin = Integer.parseInt(thisInputPin.getText());
@@ -107,27 +111,148 @@ public class CorBank extends Application {
         } else {
             JOptionPane.showMessageDialog(null, "Account already registered");
         }
-        System.out.println(arrUsername);
-        System.out.println(arrUserpin);
-        System.out.println(arrCheckingBalance);
-        System.out.println(arrSavingsBalance);
-        System.out.println(arrUsername.indexOf(inputUsername));
-        System.out.println(arrUserpin.indexOf(inputUserpin));
     }
+
+
+    public static double CheckingSessionBalance = 0.0;
+    public static double SavingsSessionBalance = 0.0;
+    public static double NewCheckingsBalance = 0.0;
+    public static double NewSavingsBalance = 0.0;
+    public static String[] typesOfAcc = {"Savings", "Checkings"};
 
     @FXML
     void withdrawClick(ActionEvent event) {
-
+        boolean end = false;
+        while (!end) {
+            String withdrawAccType = String.valueOf(JOptionPane.showInputDialog(
+                    null,
+                    "Select Account to Withdraw: ",
+                    "Withdraw Funds", JOptionPane.QUESTION_MESSAGE,
+                    null, typesOfAcc, typesOfAcc[0]));
+            double amount = Double.parseDouble(JOptionPane.showInputDialog(null, "Input amount to withdraw: "));
+            if (withdrawAccType == "Savings") {
+                try {
+                    if (amount > arrSavingsBalance.get(indexReference)) {
+                        JOptionPane.showMessageDialog(null, "Amount Greater than Balance!");
+                    } else {
+                        NewSavingsBalance = SavingsSessionBalance - amount;
+                        arrSavingsBalance.set(indexReference, NewSavingsBalance);
+                        BalanceProcessing();
+                        end = true;
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Input invalid!");
+                }
+            } else if (withdrawAccType == "Checkings") {
+                try {
+                    if (amount > arrSavingsBalance.get(indexReference)) {
+                        JOptionPane.showMessageDialog(null, "Amount Greater than Balance!");
+                    } else {
+                        NewCheckingsBalance = CheckingSessionBalance - amount;
+                        arrCheckingBalance.set(indexReference, NewCheckingsBalance);
+                        BalanceProcessing();
+                        end = true;
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Input invalid!");
+                }
+            }
+        }
     }
 
     @FXML
     void DespositClick(ActionEvent event) {
-
+        boolean end = false;
+        while (!end) {
+            String withdrawAccType = String.valueOf(JOptionPane.showInputDialog(
+                    null,
+                    "Select Account to Deposit: ",
+                    "Withdraw", JOptionPane.QUESTION_MESSAGE,
+                    null, typesOfAcc, typesOfAcc[0]));
+            double amount = Double.parseDouble(JOptionPane.showInputDialog(null, "Input amount to withdraw: "));
+            if (withdrawAccType == "Savings") {
+                try {
+                    NewSavingsBalance = SavingsSessionBalance + amount;
+                    arrSavingsBalance.set(indexReference, NewSavingsBalance);
+                    BalanceProcessing();
+                    end = true;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Input invalid!");
+                }
+            } else if (withdrawAccType == "Checkings") {
+                try {
+                    NewCheckingsBalance = CheckingSessionBalance + amount;
+                    arrCheckingBalance.set(indexReference, NewCheckingsBalance);
+                    BalanceProcessing();
+                    end = true;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Input invalid!");
+                }
+            }
+        }
     }
 
     @FXML
     void TransferClick(ActionEvent event) {
-
+        boolean end = false;
+        while (!end) {
+            String fromTransferType = String.valueOf(JOptionPane.showInputDialog(
+                    null,
+                    "Transfer Funds from: ",
+                    "Transfer Funds", JOptionPane.QUESTION_MESSAGE,
+                    null, typesOfAcc, typesOfAcc[0]));
+            double amount = Double.parseDouble(JOptionPane.showInputDialog(null, "Input amount to transfer: "));
+            if (fromTransferType == "Savings") {
+                try {
+                    if (amount > arrSavingsBalance.get(indexReference)) {
+                        JOptionPane.showMessageDialog(null, "Amount Greater than Saving Balance!");
+                    } else {
+                        NewSavingsBalance = (SavingsSessionBalance - amount);
+                        NewCheckingsBalance = (CheckingSessionBalance + amount);
+                        arrCheckingBalance.set(indexReference, NewCheckingsBalance);
+                        arrSavingsBalance.set(indexReference, NewSavingsBalance);
+                        BalanceProcessing();
+                        end = true;
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Input invalid!");
+                }
+            } else if (fromTransferType == "Checkings") {
+                try {
+                    if (amount > arrCheckingBalance.get(indexReference)) {
+                        JOptionPane.showMessageDialog(null, "Amount Greater than Checking Balance!");
+                    } else {
+                        NewCheckingsBalance = (CheckingSessionBalance - amount);
+                        NewSavingsBalance = (SavingsSessionBalance + amount);
+                        arrCheckingBalance.set(indexReference, NewCheckingsBalance);
+                        arrSavingsBalance.set(indexReference, NewSavingsBalance);
+                        BalanceProcessing();
+                        end = true;
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Input invalid!");
+                }
+            }
+        }
     }
 
+    void BalanceProcessing() {
+        CheckingSessionBalance = arrCheckingBalance.get(indexReference);
+        SavingsSessionBalance = arrSavingsBalance.get(indexReference);
+        if (CheckingSessionBalance == 0) {
+            thisCheckingBalance.setText("No Balance");
+        } else {
+            thisCheckingBalance.setText(String.valueOf(CheckingSessionBalance));
+        }
+        if (SavingsSessionBalance == 0) {
+            thisSavingsBalance.setText("No Balance");
+        } else {
+            thisSavingsBalance.setText(String.valueOf(SavingsSessionBalance));
+        }
+    }
+
+    @FXML
+    void ShowBalanceClick(ActionEvent event) {
+        BalanceProcessing();
+    }
 }
--- src/AtmMachineSimpleArray/CorBankSavings.java
+++
@@ -1,9 +0,0 @@
-package AtmMachineSimpleArray;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class CorBankSavings {
-
-}
